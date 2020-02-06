9e86176ef3c2b94a593282431a7197792be75d86
Alex Pedersen <apedersen@Titan.localdomain>
2020-02-06 13:32:09 -0800

    added function

--- app.js
+++ app.js
@@ -4,44 +4,24 @@ var userName = prompt('Welcome! What is your name?');
 alert('Welcome ' + userName);
 
 //question 1
-var question1 = prompt('Is my name Alex?');
-//console.log(question1.toLowerCase() === 'y' ||question1.toLowerCase() === 'yes')
-if (question1.toLowerCase() === 'y' ||question1.toLowerCase() === 'yes') {
-  alert('Correct!'); }
-else {
-  alert('Incorrect! Try again.'); }
+var question = prompt('Is my name Alex?');
+que();
 
 //question 2
-var question2 = prompt('Did I grow up in North Carolina?');
-//console.log(question1.toLowerCase() === 'y' ||question1.toLowerCase() === 'yes')
-if (question2.toLowerCase() === 'y' ||question2.toLowerCase() === 'yes') {
-  alert('Correct!'); }
-else {
-  alert('Incorrect! Try again.'); }
+var question = prompt('Did I grow up in North Carolina?');
+que();
 
 //question 3
-var question3 = prompt('Is my favorite color red?');
-//console.log(question1.toLowerCase() === 'y' ||question1.toLowerCase() === 'yes')
-if (question3.toLowerCase() === 'n' ||question3.toLowerCase() === 'no') {
-  alert('Correct!'); }
-else {
-  alert('Incorrect! Try again.'); }
+var question = prompt('Is my favorite color purple?');
+que();
 
 //question 4
-var question4 = prompt('Is the panthers my favorite football team?');
-//console.log(question1.toLowerCase() === 'y' ||question1.toLowerCase() === 'yes')
-if (question4.toLowerCase() === 'y' ||question4.toLowerCase() === 'yes') {
-  alert('Correct!'); }
-else {
-  alert('Incorrect! Try again.'); }
+var question = prompt('Is the panthers my favorite football team?');
+que();
 
 //question 5
-var question5 = prompt('Do I know how to play the drums?');
-//console.log(question1.toLowerCase() === 'y' ||question1.toLowerCase() === 'yes')
-if (question5.toLowerCase() === 'n' ||question5.toLowerCase() === 'no') {
-  alert('Correct!'); }
-else {
-  alert('Incorrect! Try again.'); }
+var question = prompt('Do I know how to play the guitar?');
+que();
 
 //question 6
 var tries;
@@ -98,3 +78,10 @@ for(tries = 0; tries < 4; tries++){
     }
   }
   alert('These are my favorite genres: Metal, EDM, Rock, Country, Pop, Punk');
+
+  function que(){
+    if (question.toLowerCase() === 'y' ||question.toLowerCase() === 'yes') {
+  alert('Correct!'); }
+else {
+  alert('Incorrect! Try again.'); }
+  }
