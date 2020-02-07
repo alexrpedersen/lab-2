c746c788bb9ffb9ba6e396723985c96132ff6ec7
Alex Pedersen <apedersen@Titan.localdomain>
2020-02-06 17:57:03 -0800

    restructure

--- app.js
+++ app.js
@@ -3,86 +3,109 @@
 var userName = prompt('Welcome! What is your name?');
 alert('Welcome ' + userName);
 
-//question 1
-var question = prompt('Is my name Alex?');
-que();
+var allQuestions = [
+  ['YesNo', 'Is my name Alex?', 'yes'],
+  ['YesNo', 'Did I grow up in North Carolina?', 'yes'],
+  ['YesNo', 'Is my favorite color purple?', 'yes'],
+  ['YesNo', 'Is the panthers my favorite football team?', 'yes'],
+  ['YesNo', 'Do I know how to play the guitar?', 'yes'],
+  ['HighLow', 'Pick a number between 1-10', 4],
+  ['IsInList', 'What is one of my favorite music genres?', ['metal', 'edm', 'rock', 'country', 'pop', 'punk']]
+]
 
-//question 2
-var question = prompt('Did I grow up in North Carolina?');
-que();
+// Loop over all the questions
+for(var i= 0; i < allQuestions.length; i++){
+  var questionType = allQuestions[i][0];
+  var question = allQuestions[i][1];
+  var correctAnswer = allQuestions[i][2];
 
-//question 3
-var question = prompt('Is my favorite color purple?');
-que();
-
-//question 4
-var question = prompt('Is the panthers my favorite football team?');
-que();
-
-//question 5
-var question = prompt('Do I know how to play the guitar?');
-que();
-
-//question 6
-var tries;
-for(tries = 0; tries < 4; tries++){
-  var question6 = prompt('Pick a number between 1-10');
-
-  if(question6 == 4){
-    alert('Good Job!');
-    break;
+  // Figure out how many attempts you get on the question
+  var numberOfTries = 1;
+  if(questionType === 'HighLow') {
+    numberOfTries = 4;
   }
-  else if(question6 < 4){
-    alert('Number is to low');
+  else if(questionType === 'IsInList') {
+    numberOfTries = 6;
   }
-  else if(question6 > 4){
-    alert('Number is to high');
-  }
-  }
-  if(tries > 4);
-  alert('The correct answer is 4');
-  
-  //question7
-  var answerq7 = ['metal', 'edm', 'rock', 'country', 'pop', 'punk'];
-  
-  for(var i= 0; i < answerq7.length; i++){
-  
-    var question7 = prompt('What is one of my favorite music genres?');
-    switch(question7.toLowerCase()){
-      case answerq7[0]:
-        alert('Good Job!');
-        i = 6
-        break;
-      case answerq7[1]:
-        alert('Good Job!');
-        i = 6
-        break;
-      case answerq7[2]:
-        alert('Good Job!');
-        i = 6
-        break;
-      case answerq7[3]:
-        alert('Good Job!');
-        i = 6
-        break;
-      case answerq7[4]:
-        alert('Good Job!');
-        i = 6
-        break;
-      case answerq7[5]:
-        alert('Good Job!');
-        i = 6
-        break;
-      default:
-        alert('try again');
+
+  // Ask question, get response, check if it's right
+  var isCorrect = false;
+  for(var j=0; j < numberOfTries; j++){
+    var response = prompt(question);
+    isCorrect = isAnswerCorrect(questionType, response, correctAnswer);
+    if(isCorrect){
+      break;
     }
   }
-  alert('These are my favorite genres: Metal, EDM, Rock, Country, Pop, Punk');
 
-  // Function for questions 1-5
-  function que(){
-    if (question.toLowerCase() === 'y' ||question.toLowerCase() === 'yes') {
-  alert('Correct!'); }
-    else {
-  alert('Incorrect! Try again.'); }
+  // For IsInList questions, give them them list if they fail to guess
+  if(questionType === 'IsInList' && !isCorrect)
+  {
+    alert('Sorry, correct answers were:' + correctAnswer);
+  }
+}
+alert('Thanks for playing!');
+
+function isYesNoAnswerCorrect(response, correctAnswer){
+  var responseFirstLetterLowercase = response.toLowerCase()[0];
+  var correctAnswerFirstLetterLowercase = correctAnswer.toLowerCase()[0];
+  var isCorrect = responseFirstLetterLowercase === correctAnswerFirstLetterLowercase;
+  if(isCorrect) {
+    alert('You\'re right!');
+    return true;
+  }
+  else {
+    alert('You suck! Try again!');
+    return false;
+  }
+}
+
+function isHighLowAnswerCorrect(response, correctAnswer){
+  if(response == correctAnswer){
+    alert('You are right!');
+    return true;
+  }
+  else if(response < correctAnswer) {
+    alert('Number is too low');
+    return false;
+  }
+  else if(response > correctAnswer) {
+    alert('Number is too high');
+    return false;
+  }
+}
+
+function isIsInListAnswerCorrect(response, correctAnswer){
+  var isInList = false;
+  for(var i= 0; i < correctAnswer.length; i++){
+    if(response.toLowerCase() === correctAnswer[i])
+    {
+      isInList = true;  // Mark that you found it in the list
+      break; // And quit looking
+    }
+  }
+  if(isInList){
+    alert('Correct!');
+    return true;
+  }
+  else {
+    alert('Nope, guess again!');
+    return false;
+  }
+}
+
+  function isAnswerCorrect(questionType, response, correctAnswer){
+    var isCorrect = false;
+    switch(questionType){
+      case 'YesNo':
+        isCorrect = isYesNoAnswerCorrect(response, correctAnswer);
+        break;
+      case 'HighLow':
+        isCorrect = isHighLowAnswerCorrect(response, correctAnswer);
+        break;
+      case 'IsInList':
+        isCorrect = isIsInListAnswerCorrect(response, correctAnswer);
+        break;
+    }
+    return isCorrect;
   }
